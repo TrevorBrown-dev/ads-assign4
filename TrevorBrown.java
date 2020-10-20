18222055d78f0fbd53cd114f6da0e75870b7115e
Trevor Brown <trevorbrown.dev@gmail.com>
2020-10-20 00:31:47 -0400

    added splay operation

--- TrevorBrown.java
+++ TrevorBrown.java
@@ -4,82 +4,58 @@ import java.util.Stack;
 
 public class TrevorBrown {
     public static void main(String[] args) {
-        BST<Integer> t = new BST<>();
 
-        t.insert(8);
-        t.insert(3);
-        t.insert(1);
-        t.insert(6);
-        t.insert(4);
-        t.insert(7);
+        BTNode t1 = new BTNode(6);
+        BTNode t2 = new BTNode(2);
+        BTNode t3 = new BTNode(4);
+        BTNode t4 = new BTNode(3);
+        t1.setLeft(t2);
+        t2.setRight(t3);
+        t3.setLeft(t4);
+        BTNode[] path = new BTNode[] { t1, t2, t3, t4 };
 
-        // 1 5 6
+        Stack<BTNode> s = new Stack<>();
+        for (BTNode n : path) {
+            s.push(n);
+            while (s.size() > 1) {
+                BTNode n1 = s.pop();
+                BTNode n2 = s.pop();
+                BTNode n3 = s.pop();
+
+            }
+        }
+
+        BST t = new BST(t1);
         System.out.println(t);
 
-        System.out.println("Found: " + t.search(9));
+        // 1 5 6
+        // System.out.println(t);
+
+        // System.out.println("Found: " + t.search(9));
     }
 }
 
-abstract class SplayTree<T extends Comparable<T>> {
-    /**
-     * 
-     * @param root   The current node in the iteration
-     * @param target the node we are searching for
-     * @return an array of nodes where the first element is the predecessor.
-     */
-    protected List<BTNode<T>> getUniquePath(BTNode<T> root, BTNode<T> target) {
-        if (root == null)
-            return null;
-
-        List<BTNode<T>> path = new ArrayList<>();
-        path.add(null);
-        getUniquePath(root, target, path);
-        // path.add(path.get(0));
-        return path;
+interface Dictionary {
+    private void insert(BTNode target, BTNode newNode) {
     }
 
-    protected void getUniquePath(BTNode<T> root, BTNode<T> target, List<BTNode<T>> path) {
-        if (root == null)
-            return;
-        if (root.equals(target)) {
-            path.set(0, root);
-            return;
-        }
+    public void insert(BTNode newNode);
 
-        path.add(root);
-        path.set(0, root);
+    public int search(int target);
 
-        if (root.compareTo(target) > 0)
-            getUniquePath(root.getLeft(), target, path);
-        else {
-            if (root.getRight() != null)
-                path.set(0, root.getRight());
-            getUniquePath(root.getRight(), target, path);
-        }
-
-    }
-
-    protected abstract void splay(BTNode<T> node, T data);
-
+    public int delete(BTNode target);
 }
 
-interface Dictionary<T extends Comparable<T>> {
-    private void insert(BTNode<T> target, BTNode<T> newNode) {
-    }
-
-    public void insert(BTNode<T> newNode);
-
-    public T search(T target);
-
-    public T delete(BTNode<T> target);
-}
-
-class BST<T extends Comparable<T>> extends SplayTree<T> implements Dictionary<T> {
-    private BTNode<T> root;
+class BST {
+    private BTNode root;
 
     // #region CONSTRUCTORS
-    public BST(T data) {
-        root = new BTNode<T>(data);
+    public BST(int data) {
+        this(new BTNode(data));
+    }
+
+    public BST(BTNode root) {
+        this.root = root;
     }
 
     public BST() {
@@ -87,65 +63,88 @@ class BST<T extends Comparable<T>> extends SplayTree<T> implements Dictionary<T>
     }
     // #endregion
 
-    protected void splay(BTNode<T> node, T key) {
-
+    private BTNode rightRotate(BTNode x) {
+        BTNode y = x.getLeft();
+        x.setLeft(y.getRight());
+        y.setRight(x);
+        return y;
     }
 
-    // #region INSERT
-    public void insert(T data) {
-        try {
-            root = insert(root, new BTNode<T>(data));
-        } catch (Exception e) {
-            System.out.println("Duplicate Keys not allowed!");
-            return;
+    private BTNode leftRotate(BTNode x) {
+        BTNode y = x.getRight();
+        x.setRight(y.getLeft());
+        y.setLeft(x);
+        return y;
+    }
+
+    private BTNode splay(BTNode root, int key) {
+        // Base cases: root is null or
+        // key is present at root
+        if (root == null || root.getData() == key)
+            return root;
+
+        // Key lies in left subtree
+        if (root.getData() > key) {
+            // Key is not in tree, we are done
+            if (root.getLeft() == null)
+                return root;
+
+            // Zig-Zig (Left Left)
+            if (root.getLeft().getData() > key) {
+                // First recursively bring the
+                // key as root of left-left
+                root.getLeft().setLeft(splay(root.getLeft().getLeft(), key));
+
+                // Do first rotation for root,
+                // second rotation is done after else
+                root = rightRotate(root);
+            } else if (root.getLeft().getData() < key) // Zig-Zag (Left Right)
+            {
+                // First recursively bring
+                // the key as root of left-right
+                root.getLeft().setRight(splay(root.getLeft().getRight(), key));
+
+                // Do first rotation for root.left
+                if (root.getLeft().getRight() != null)
+                    root.setLeft(leftRotate(root.getLeft()));
+            }
+
+            // Do second rotation for root
+            return (root.getLeft() == null) ? root : rightRotate(root);
+        } else // Key lies in right subtree
+        {
+            // Key is not in tree, we are done
+            if (root.getRight() == null)
+                return root;
+
+            // Zag-Zig (Right Left)
+            if (root.getRight().getData() > key) {
+                // Bring the key as root of right-left
+                root.getRight().setLeft(splay(root.getRight().getLeft(), key));
+
+                // Do first rotation for root.right
+                if (root.getRight().getLeft() != null)
+                    root.setRight(rightRotate(root.getRight()));
+            } else if (root.getRight().getData() < key)// Zag-Zag (Right Right)
+            {
+                // Bring the key as root of
+                // right-right and do first rotation
+                root.getRight().setRight(splay(root.getRight().getRight(), key));
+                root = leftRotate(root);
+            }
+
+            // Do second rotation for root
+            return (root.getRight() == null) ? root : leftRotate(root);
         }
-        System.out.println("Key successfully inserted!");
+
     }
 
-    private BTNode<T> insert(BTNode<T> target, BTNode<T> newNode) throws Exception {
-        if (target == null) {
-            return newNode;
-        }
-        if (target.compareTo(newNode) > 0) {
-            target.setLeft(insert(target.getLeft(), newNode));
-        } else if (target.compareTo(newNode) < 0) {
-            target.setRight(insert(target.getRight(), newNode));
-        } else {
-            throw new Exception("Duplicate Keys not allowed!");
-        }
-
-        return target;
+    public BTNode search(int key) {
+        return splay(root, key);
     }
 
-    // #endregion
-    // #region SEARCH
-    public T search(T target) {
-        return (root == null) ? null : search(new BTNode<T>(target));
-    }
-
-    private T search(BTNode<T> target) {
-        ArrayList<BTNode<T>> l = (ArrayList<BTNode<T>>) getUniquePath(root, target);
-        BTNode<T> predecessor = l.remove(0);
-
-        if (predecessor == null)
-            return null;
-
-        if (!predecessor.equals(target)) {
-            System.out.println("Key not found!");
-        }
-
-        // l[0] is predecessor but target is not in tree.
-        // splay at each step
-        // splay at predecessor
-        System.out.println(l);
-        return predecessor.getData();
-    }
-    // #endregion
-
-    public T delete(BTNode<T> target) {
-
-        // If not found
-        return null;
+    private BTNode search(BTNode root, int key) {
+        return splay(root, key);
     }
 
     // #region UTILS
@@ -155,7 +154,7 @@ class BST<T extends Comparable<T>> extends SplayTree<T> implements Dictionary<T>
         return sb.toString();
     }
 
-    private void inOrder(StringBuilder sb, BTNode<T> root) {
+    private void inOrder(StringBuilder sb, BTNode root) {
 
         if (root == null)
             return;
@@ -174,52 +173,52 @@ class BST<T extends Comparable<T>> extends SplayTree<T> implements Dictionary<T>
 
 }
 
-class BTNode<T extends Comparable<T>> implements Comparable<BTNode<T>> {
+class BTNode {
 
-    private T data;
-    private BTNode<T> left;
-    private BTNode<T> right;
+    private int data;
+    private BTNode left;
+    private BTNode right;
 
-    public BTNode(T data) {
+    public BTNode(int data) {
         this.data = data;
     }
 
     // #region Getters and Setters
-    public T getData() {
+    public int getData() {
         return data;
     }
 
-    public BTNode<T> getLeft() {
+    public BTNode getLeft() {
         return left;
     }
 
-    public BTNode<T> getRight() {
+    public BTNode getRight() {
         return right;
     }
 
-    public void setData(T data) {
+    public void setData(int data) {
         this.data = data;
     }
 
-    public void setLeft(BTNode<T> left) {
+    public void setLeft(BTNode left) {
         this.left = left;
     }
 
-    public void setRight(BTNode<T> right) {
+    public void setRight(BTNode right) {
         this.right = right;
     }
 
-    public int compareTo(BTNode<T> other) {
-        return data.compareTo(other.data);
+    public int compareTo(BTNode other) {
+        return data - other.data;
     }
 
-    public boolean equals(BTNode<T> other) {
-        return data.equals(other.data);
+    public boolean equals(BTNode other) {
+        return data == other.data;
     }
 
     // #endregion
     public String toString() {
-        return data.toString();
+        return data + "";
     }
 
 }
